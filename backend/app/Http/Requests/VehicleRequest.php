0e8528767cfa2e81dca299f46c34ecfd240a21eb
Firgisotya <zzuuddiinn28@gmail.com>
2024-02-08 22:54:46 +0700

    feat: add request

---
+++ backend/app/Http/Requests/VehicleRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class VehicleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'category' => 'required',
+            'type' => 'required',
+            'name' => 'required|string|max:100',
+            'police_number' => 'required|string|max:20',
+            'color' => 'required|string|max:20',
+            'year' => 'required|string|max:4',
+            'stock' => 'required|integer',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 422));
+    }
+}
